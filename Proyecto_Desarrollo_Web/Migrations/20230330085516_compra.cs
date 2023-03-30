e7a66db5fc8202c941542f61c87cad18c310ccea
Eduardo Ramirez <123214616+Ramirez30@users.noreply.github.com>
2023-03-30 15:13:23 -0600

    migracion

---
+++ Proyecto_Desarrollo_Web/Migrations/20230330085516_compra.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_Desarrollo_Web.Migrations
+{
+    public partial class compra : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CompraEncabezado",
+                columns: table => new
+                {
+                    CompraEncabezadoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProveedorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FechaSolicitud = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    FechaEntrega = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NumeroFactura = table.Column<string>(type: "varchar(50)", nullable: true),
+                    Estado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompraEncabezado", x => x.CompraEncabezadoId);
+                    table.ForeignKey(
+                        name: "FK_CompraEncabezado_Proveedor_ProveedorId",
+                        column: x => x.ProveedorId,
+                        principalTable: "Proveedor",
+                        principalColumn: "ProveedorId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompraEncabezado_ProveedorId",
+                table: "CompraEncabezado",
+                column: "ProveedorId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompraEncabezado");
+        }
+    }
+}
---
+++ Proyecto_Desarrollo_Web/Migrations/20230330104159_compraencabezado2.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proyecto_Desarrollo_Web.Migrations
+{
+    public partial class compraencabezado2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
